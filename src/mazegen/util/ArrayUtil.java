13274920a9166853b3e9a8f1104ec2da1fe41df1
Radon Rosborough <radon.neon@gmail.com>
2016-06-02 10:52:52 -0600

    Add ArrayUtil.horizontalJoin

--- src/mazegen/util/ArrayUtil.java
+++ src/mazegen/util/ArrayUtil.java
@@ -1,7 +1,9 @@
 // Copyright (c) 2016 by Radon Rosborough. All rights reserved.
 package mazegen.util;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class ArrayUtil {
 
@@ -25,6 +27,23 @@ public final class ArrayUtil {
 
     public static <T> T last(List<T> list) {
         return list.get(list.size() - 1);
+    }
+
+    public static String horizontalJoin(String delimiter, List<String> strings) {
+        return strings
+                .stream()
+                .map(str -> Arrays.asList(str.split("\\n")))
+                .reduce((l1, l2) -> {
+                    if (l1.size() != l2.size())
+                        throw new IllegalArgumentException("each string must have the same number of lines");
+                    for (int i=0; i<l1.size(); i++) {
+                        l1.set(i, l1.get(i) + delimiter + l2.get(i));
+                    }
+                    return l1;
+                })
+                .get()
+                .stream()
+                .collect(Collectors.joining("\n"));
     }
 
 }
