0acdc047490ad523001bd198f5a7a6440d92c90b
Radon Rosborough <radon.neon@gmail.com>
2016-06-02 18:13:58 -0600

    Add SolutionMaze and related interfaces
    Add the SolutionMaze, GeneratingSolutionMaze, and
    ReversibleGeneratingSolutionMaze interfaces.

---
+++ src/mazegen/maze/ReversibleGeneratingSolutionMaze.java
@@ -0,0 +1,4 @@
+// Copyright (c) 2016 by Radon Rosborough. All rights reserved.
+package mazegen.maze;
+
+public interface ReversibleGeneratingSolutionMaze extends ReversibleGeneratingMaze, SolutionMaze {}
---
+++ src/mazegen/maze/SolutionMaze.java
@@ -0,0 +1,9 @@
+// Copyright (c) 2016 by Radon Rosborough. All rights reserved.
+package mazegen.maze;
+
+public interface SolutionMaze extends Maze {
+
+    public void showSolution();
+    public void hideSolution();
+
+}
